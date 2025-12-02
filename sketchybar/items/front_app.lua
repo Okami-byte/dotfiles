2d3522b4155164b0a29a11ea40992380bdee67f2
Okami <49962728+Okami-byte@users.noreply.github.com>
2025-12-02 15:03:24 -0500

    Add minimal rose-pine sketchybar config

--- sketchybar/items/front_app.lua
+++ sketchybar/items/front_app.lua
@@ -1,117 +1,51 @@
-local colors = require("colors")
-local settings = require("settings")
-local app_icons = require("helpers.app_icons")
+local colors = require("colors").sections
+local icon_map = require "helpers.icon_map"
 
-sbar.add("event", "window_focus")
-sbar.add("event", "title_change")
-
-local apps = sbar.add("bracket", "apps", {}, {
-	position = "left",
-	icon = {
-		padding_right = 15,
-		padding_left = 8,
-		color = colors.arise,
-		font = "sketchybar-app-font:Regular:16.0",
-		y_offset = -1,
-	},
-	label = {
-		font = {
-			style = settings.font.style_map["Black"],
-			size = 12.0,
-		},
-	},
-	background = {
-		color = colors.bg2,
-		border_color = colors.bg1,
-		border_width = 2,
-	},
+sbar.add("item", "e_bracket.padding", {
+  position = "e",
+  background = {
+    drawing = false,
+  },
+  width = 12,
+})
+local front_app = sbar.add("item", "front_app", {
+  position = "e",
+  display = "active",
+  icon = {
+    padding_left = 8,
+    font = "sketchybar-app-font:Regular:14.0",
+    string = "app",
+    padding_right = 4,
+    color = colors.front_app.icon,
+  },
+  label = {
+    pading_left = 4,
+    padding_right = 8,
+    color = colors.front_app.label,
+  },
+  background = {
+    drawing = false,
+  },
+  updates = true,
 })
 
--- Function to truncate strings to a max length and add dots if needed
-local function truncate_string(str, max_length)
-	if #str > max_length then
-		return str:sub(1, max_length) .. "…"
-	else
-		return str
-	end
-end
+sbar.add("bracket", { "/front\\_app/" }, {
+  background = {
+    color = colors.bracket.bg,
+    height = 34,
+    border_color = colors.bracket.border,
+    corner_radius = 9999,
+  },
+})
 
-local function update_windows(windows)
-	sbar.remove("/apps.\\.*/")
-
-	-- Filter windows to exclude "Arc", "Fivenotes(WIP)" and "kitty" without titles
-	local filtered_windows = {}
-	for _, window in ipairs(windows) do
-		if
-			not (
-				(
-					window["app"] == "Zen Browser"
-					or window["app"] == "Twilight"
-					or window["app"] == "Firefox"
-					or window["app"] == "FiveNotes"
-					or window["app"] == "kitty"
-					or window["app"] == "WezTerm"
-					or window["app"] == "Ghostty"
-				) and (window["title"] == nil or window["title"] == "")
-			)
-		then
-			table.insert(filtered_windows, window)
-		end
-	end
-
-	local max_length
-
-	-- Filter titles if too many windows
-	local count = #filtered_windows
-	if count > 4 then
-		max_length = nil -- Show only the app name
-	elseif count > 3 then
-		max_length = 5
-	elseif count > 2 then
-		max_length = 15
-	elseif count > 1 then
-		max_length = 25
-	else
-		max_length = 50
-	end
-
-	for _, window in ipairs(filtered_windows) do
-		local window_label
-		if max_length then
-			window_label = truncate_string(window["app"], max_length)
-		else
-			window_label = window["app"]
-		end
-
-		-- Fetch the icon for the app
-		local icon_lookup = app_icons[window["app"]]
-		local icon = icon_lookup or app_icons["default"]
-
-		sbar.add("item", "apps." .. window["id"], {
-			label = {
-				string = window_label,
-				highlight = window["has-focus"],
-				color = colors.grey,
-				highlight_color = colors.pink,
-			},
-			icon = {
-				string = icon,
-				font = "sketchybar-app-font:Regular:16.0",
-				color = colors.arise,
-			},
-			padding_right = 2,
-			click_script = "yabai -m window --focus " .. window["id"],
-		})
-	end
-end
-
-local function get_apps()
-	sbar.exec("yabai -m query --windows id,title,app,has-focus --space", function(output)
-		update_windows(output)
-	end)
-end
-
-apps:subscribe("space_change", get_apps)
-apps:subscribe("front_app_changed", get_apps)
-apps:subscribe("title_change", get_apps)
-apps:subscribe("window_focus", get_apps)
+front_app:subscribe("front_app_switched", function(env)
+  local app = env.INFO
+  front_app:set {
+    icon = {
+      string = (icon_map[app] or icon_map["default"]),
+    },
+    label = {
+      string = env.INFO,
+    },
+  }
+end)
--- sketchybar/items/init.lua
+++ sketchybar/items/init.lua
@@ -1,11 +1,18 @@
--- Left items (L to R)
-require("items.apple")
-require("items.menus")
-require("items.spaces")
-require("items.front_app")
+local colors = require("colors").sections
+--left
+require "items.apple"
+require "items.menus"
+require "items.spaces_yabai"
+require "items.front_app_yabai"
 
--- Right items (R to L)
-require("items.calendar")
-require("items.widgets")
--- require("items.wallpaper")
-require("items.media")
+sbar.add("bracket", { "/system\\..*/" }, {
+  background = {
+    height = 34,
+    color = colors.bracket.bg,
+    border_color = colors.bracket.border,
+    corner_radius = 9999,
+  },
+})
+
+--right (reverse order)
+require "items.widgets"
