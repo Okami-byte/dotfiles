52c7d96707e5b3966f1badd99ea3435be9ac3c29
Okami <49962728+Okami-byte@users.noreply.github.com>
2026-01-06 13:43:37 -0500

    Add pomodoro timer for study sessions

---
+++ sketchybar/states/pomodoro_state.lua
@@ -0,0 +1,13 @@
+return {
+  config = {
+    focus = 25,
+    short = 5,
+    long = 15,
+  },
+  state = {
+    phase = "focus",
+    focus_completed = 1,
+    remaining = 1500,
+    running = false,
+  },
+}
