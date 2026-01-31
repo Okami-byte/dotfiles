34e707974a9a39280a698b07288e6aa75cd7b1c1
Okami <49962728+Okami-byte@users.noreply.github.com>
2026-01-31 11:44:18 -0500

    Fixed displaygroups to fit my setup

--- sketchybar/plugins/display/script.sh
+++ sketchybar/plugins/display/script.sh
@@ -6,47 +6,51 @@ source $RELPATH/log_handler.sh
 ## Defaults
 # List of possible display groups
 displaygroups=(
-	"Built-in"
-	"Built-in + External"
-	"Dual External"
+  "Built-in"
+  "External"
+  "Built-in + External"
+  "Dual External"
 )
 
 # Default item
-ICON="􀢹"
+ICON="􀙗"
 
 ## Private functions
 match_displayGroup() {
-	for displayGroup in "${displaygroups[@]}"; do
-		if [[ "$(betterdisplaycli get --name="$displayGroup" --active)" == "on" ]]; then
-			# Get Active display groupe name
+  for displayGroup in "${displaygroups[@]}"; do
+    if [[ "$(betterdisplaycli get --name="$displayGroup" --active)" == "on" ]]; then
+      # Get Active display group name
 
-			# Assign icon depending on active display group
-			case "$displayGroup" in
-			'Built-in')
-				ICON="􁈸"
-				;;
-			'Built-in + External')
-				ICON="􂤓"
-				;;
-			'Dual External')
-				ICON="􀨧"
-				;;
-			*)
-				sendErr "Unrecognized displayGroup $displayGroup" "debug"
-				;;
-			esac
-		fi
-	done
+      # Assign icon depending on active display group
+      case "$displayGroup" in
+      'Built-in')
+        ICON="􁈸"
+        ;;
+      'External')
+        ICON="􀢹"
+        ;;
+      'Built-in + External')
+        ICON="􂤓"
+        ;;
+      'Dual External')
+        ICON="􀨧"
+        ;;
+      *)
+        sendErr "Unrecognized displayGroup $displayGroup" "debug"
+        ;;
+      esac
+    fi
+  done
 
-	sketchybar --set $NAME icon="$ICON" drawing="on"
+  sketchybar --set $NAME icon="$ICON" drawing="on"
 
-	sendLog "Updated displayGroup icon" "vomit"
+  sendLog "Updated displayGroup icon" "vomit"
 }
 
 ## Main logic
 if [[ -n "$(pgrep "BetterDisplay")" ]]; then
-	match_displayGroup
+  match_displayGroup
 else
-	sendWarn "BetterDisplay App is not running, hiding display item" "info"
-	sketchybar --set $NAME drawing="off"
+  sendWarn "BetterDisplay App is not running, hiding display item" "info"
+  sketchybar --set $NAME drawing="off"
 fi
