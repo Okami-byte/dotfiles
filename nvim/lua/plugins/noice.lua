d8be9c2a58c0b7c122c9a9af86a8ff47073aa220
Okami <49962728+Okami-byte@users.noreply.github.com>
2026-03-03 21:54:33 -0500

    Updates

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,13 +5,13 @@ return {
     "MunifTanjim/nui.nvim",
   },
   opts = {
-    cmdline = { enabled = true },
-    messages = { enabled = true },
+    cmdline = { enabled = false },
+    messages = { enabled = false },
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = false, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
