7dc52637b8e551f6cb6b15ca80c002dd3a96b89a
Okami <49962728+Okami-byte@users.noreply.github.com>
2026-05-27 09:40:53 -0400

    Small updates

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -4,7 +4,7 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
 
-  enabled = true,
+  enabled = false,
 
   config = function()
     require("render-markdown").setup({
@@ -265,34 +265,25 @@ return {
       -- Checkboxes are a special instance of a 'list_item' that start with a 'shortcut_link'
       -- There are two special states for unchecked & checked defined in the markdown grammar
       checkbox = {
-        -- Turn on / off checkbox state rendering
         enabled = true,
-        -- Determines how icons fill the available space:
-        --  inline:  underlying text is concealed resulting in a left aligned icon
-        --  overlay: result is left padded with spaces to hide any additional text
-        position = "inline",
+        render_modes = false,
+        bullet = false,
+        left_pad = 0,
+        right_pad = 1,
         unchecked = {
-          -- Replaces '[ ]' of 'task_list_marker_unchecked'
           icon = "󰄱 ",
-          -- Highlight for the unchecked icon
           highlight = "RenderMarkdownUnchecked",
+          scope_highlight = nil,
         },
         checked = {
-          -- Replaces '[x]' of 'task_list_marker_checked'
           icon = "󰱒 ",
-          -- Highlight for the checked icon
           highlight = "RenderMarkdownChecked",
+          scope_highlight = "@markup.strikethrough",
         },
-        -- Define custom checkbox states, more involved as they are not part of the markdown grammar
-        -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks
-        -- Can specify as many additional states as you like following the 'todo' pattern below
-        --   The key in this case 'todo' is for healthcheck and to allow users to change its values
-        --   'raw':       Matched against the raw text of a 'shortcut_link'
-        --   'rendered':  Replaces the 'raw' value when rendering
-        --   'highlight': Highlight for the 'rendered' icon
         custom = {
-          todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo" },
+          todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
         },
+        scope_priority = nil,
       },
       quote = {
         -- Turn on / off block quote & callout rendering
